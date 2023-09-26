e95f5c6eca740f7a684ac46af85788b261e493d8
SetuSingh17 <setusingh665@gmail.com>
2023-09-26 12:06:19 +0530

    first commit

--- src/main/java/com/setu/restcon.java
+++ src/main/java/com/setu/restcon.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class restcon {
 	@GetMapping("/get")
-	public String show() {
-		return "Welcome to spring boot application";
+	public String toString() {
+		
+		String firstName = "Setu";
+		String lastName = "Singh";
+		String email = "setusingh665@gmail.com";
+		String work = "Amdocs";
+	    return String.format( firstName + " " +  lastName + "  " + email + " " + work );
 	}
 	
-
 }
